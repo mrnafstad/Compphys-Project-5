eb42b3fbe499539c38e37a49ff01775b7fac36f4
Halvor Nafstad <Halvor Nafstad>
2016-12-01 17:21:21 +0100

    Now cluster should generate a random-ish cluster. Next step to maybe redesign the random numbers to be gaussian, then the rest is straight forward I believe!

--- Code/cluster.cpp
+++ Code/cluster.cpp
@@ -4,8 +4,10 @@
 #include <cstring>
 #include <fstream>
 #include <time.h>
+#include <vector>
 #include "planet.h"
 #include "solver.h"
+#include "lib.h"
 using namespace std;
 
 
@@ -14,12 +16,25 @@ int main ( int argc, char * argv[] )
 {
 	//NB N is now the number of initial bodies, dt is the number of integration points!
 	int dim = 3, dt = atoi(argv[1]), N = atoi(argv[2]);
+	long idum = -1;
 
 	double final_time =  atof(argv[3]), R0 = atof(argv[4]), M0 = 1.0;
-	string particle = "particle";
+	double mass, R, x, y, z, theta, phi, vx = 0, vy = 0, vz = 0;
+
+	vector<planet> cluster;
 
 	for ( int i = 0; i <= N; i++) {
-		string thisparticle = particle + string(itoa(i));
+		R = ran1(&idum)*R0;
+		theta = ran1(&idum)*2*M_PI;
+		phi = ran1(&idum)*M_PI;
+		x = R*cos(theta)*sin(phi);
+		y = R*sin(theta)*sin(phi);
+		z = R*cos(phi);
+
+		mass = ran1(&idum);
+
+		planet thistest( mass, x, y, z, vx, vy, vz );
+		cluster.push_back( thistest );
 	}
 
 
