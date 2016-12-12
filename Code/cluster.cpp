f72b10c0b1dcec01124915a887633aca7f0e5f8a
Myrvixen <tommy_myrvik@hotmail.com>
2016-12-12 16:02:24 +0100

    Add files via upload

--- Code/cluster.cpp
+++ Code/cluster.cpp
@@ -36,9 +36,9 @@ double gaussian_deviate(long * idum)
 
 void density_profile( solver allplanets, double shell_size, int N, double R0 )
 {
-	int thisshell, total = 0;
+	int thisshell, total = 0, bound = 0;
 	double massthis;
-	double r = 0;
+	double r = 0, r_average1 = 0, r_average2 = 0, stddev1 = 0, stddev2 = 0;
 
 	FILE *dens;
 	char DensOut[30];
@@ -54,24 +54,40 @@ void density_profile( solver allplanets, double shell_size, int N, double R0 )
 		for ( int i = 0; i <= allplanets.total_planets; i++ ) {
 			planet thisplanet = allplanets.all_planets[i];
 
+			if(!thisplanet.bound) continue;
+
 			double radius = 0, rad;
 
 			for (int j = 0; j < 3; j++) radius += thisplanet.position[j]*thisplanet.position[j];
 			rad  = sqrt(radius);
-			//std:cout << rad << std::endl;
+
+			//Adds together radi (only one time, hence e.g. r = 0.0)
+			if (r == 0.0){
+				r_average1 += rad;
+				bound += 1;
+				if(rad < R0 + shell_size) total += 1;
+			}
 
 			if ( (rad > r) && (rad < r + shell_size) ) {
 				thisshell += 1;
 				massthis += thisplanet.mass;
+				r_average2 += rad;
 			}
 		}
 
-		total += thisshell;
+		stddev1 += thisshell*pow(((r + shell_size/2) - r_average1/bound), 2);
+		stddev2 += thisshell*pow(((r + shell_size/2) - r_average2/total), 2);
 		fprintf(dens, "%f %i \n", r, thisshell);
 		r += shell_size;
 	
 	}
-	std::cout << total << std::endl;
+
+	printf("\nStars within R0: %i \n", total);
+	printf("Bound stars: %i \n", bound);
+	printf("Average radius (stars inside R0): %.3f ly\n", r_average2/total);
+	printf("Average radius (all bound stars): %.3f ly\n", r_average1/bound);
+	printf("Standard deviation (starts inside R0): %.3f ly\n", sqrt(stddev2/total));
+	printf("Standard deviation (all bound stars): %.3f ly\n", sqrt(stddev1/bound));
 	fclose(dens);
 }
 
@@ -108,14 +124,23 @@ int main ( int argc, char * argv[] )
 		return 0;
 	}
 	else{
-		dt = atof(argv[4]);
 		N = atoi(argv[1]);
-		final_time =  atof(argv[3]);
 		R0 = atof(argv[2]);
+		final_time = atof(argv[3]);
+		dt = atof(argv[4]);
 	}
 	shell_size = R0/(double)100;
+
 	bool energy = true;
+<<<<<<< HEAD
+	bool smoothing = true;
+	bool constantMass = true;
+
+	double M0 = 1000;  // solar masses 
+	double mu = M0/N;  // average mass per particle
+=======
 	bool smoothing = false;
+>>>>>>> eb4e2444095254ab47471ac2a9f0ae4299334cc4
 
 	solver system_VV(R0);
 
@@ -127,8 +152,13 @@ int main ( int argc, char * argv[] )
 		y = R*sin(theta)*sin(phi);
 		z = R*cos(phi);
 
-		mass = gaussian_deviate(&idum) + 10;
-		//printf("%lf %lf %lf %lf\n", mass, x, y, z);
+		if(constantMass){
+			// 100/N adjusting stddev so that mass is never negative
+			mass = 100/(double)N*gaussian_deviate(&idum) + mu;
+		}
+		else{
+			mass = gaussian_deviate(&idum) + 10;
+		}
 
 		planet thistest( mass, x, y, z, vx, vy, vz );
 		system_VV.add( thistest );
